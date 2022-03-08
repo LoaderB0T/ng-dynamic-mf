65c8924166dd2420d56d5e36f8e3fc50a36761b8
Janik Schumacher <rndmb0t@gmail.com>
2022-03-08 22:20:41 +0100

    Fixed hardcoded module name

--- projects/ng-dynamic-mf/src/lib/translations/loaded-translations.service.ts
+++ projects/ng-dynamic-mf/src/lib/translations/loaded-translations.service.ts
@@ -19,7 +19,7 @@ export class DynamicTranslationService {
   private readonly _loadedTranslations = new BehaviorSubject<TranslationSet>({});
   public readonly loadedTranslations$ = this._loadedTranslations.asObservable();
 
-  public async registerTranslations(locales: string[], resolver: (locale: string) => string): Promise<void> {
+  public async registerTranslations(locales: string[], resolver: (locale: string) => string, moduleName: string): Promise<void> {
     if (!this._translateService) {
       throw new Error(
         'DynamicTranslationService: TranslateService not found. Make sure you have @ngx-translate/core installed and called setTranslateService()'
@@ -27,7 +27,7 @@ export class DynamicTranslationService {
     }
     const result: { locale: string; translations: TranslationType }[] = [];
     const promise = locales.map(async locale => {
-      const resourceUrl = resourceMapper('core', resolver(locale));
+      const resourceUrl = resourceMapper(moduleName, resolver(locale));
       const response = await fetch(resourceUrl);
       const json = (await response.json()) as TranslationType;
       return result.push({ translations: json, locale });
