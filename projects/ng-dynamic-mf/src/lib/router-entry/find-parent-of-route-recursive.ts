ad693305503e115cd228740047504945372a2387
Janik Schumacher <rndmb0t@gmail.com>
2022-03-08 18:41:17 +0100

    fixed validation

--- projects/ng-dynamic-mf/src/lib/router-entry/find-parent-of-route-recursive.ts
+++ projects/ng-dynamic-mf/src/lib/router-entry/find-parent-of-route-recursive.ts
@@ -1,20 +1,20 @@
 import { Route, Routes } from '@angular/router';
 
-export const findParentOfRouteRecursive = (routes: Routes, path: string): Route | undefined => {
+export const findParentOfRouteRecursive = (routes: Routes, path: string, level = 0): Route | undefined => {
   for (const route of routes) {
     if (route.path === path) {
+      if (level === 0) {
+        throw new Error(
+          "The entry point cannot be at the root of the routing configuration.\
+           You don't need to use this method if you are using the entry point at the root of the routing configuration."
+        );
+      }
       return route;
     }
 
     if (route.children) {
-      const found = findParentOfRouteRecursive(route.children, path);
+      const found = findParentOfRouteRecursive(route.children, path, level + 1);
       if (found) {
-        if (found === route) {
-          throw new Error(
-            "The entry point cannot be at the root of the routing configuration.\
-             You don't need to use this method if you are using the entry point at the root of the routing configuration."
-          );
-        }
         return route;
       }
     }
