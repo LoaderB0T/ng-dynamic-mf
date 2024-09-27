97e7c6f9055f53b7206083a82962a78891d54bc6
Janik Schumacher <rndmb0t@gmail.com>
2024-09-27 11:30:16 +0200

    fix: optional schema

--- projects/ng-dynamic-mf/src/lib/models/module-definitions.type.ts
+++ projects/ng-dynamic-mf/src/lib/models/module-definitions.type.ts
@@ -4,7 +4,7 @@ export type ModuleDefinitions = {
   /**
    * The schema of the module definition. Should be 'https://raw.githubusercontent.com/LoaderB0T/ng-dynamic-mf/refs/heads/main/schema.json' or similar.
    */
-  $schema: string;
+  $schema?: string;
   /**
    * The modules that should be loaded from the host application.
    */
