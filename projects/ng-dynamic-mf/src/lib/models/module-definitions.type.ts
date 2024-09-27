476eb61a2c71806ad774598873f638915033d8a8
Janik Schumacher <rndmb0t@gmail.com>
2024-09-27 11:28:37 +0200

    fix: wrong place for schema

--- projects/ng-dynamic-mf/src/lib/models/module-definitions.type.ts
+++ projects/ng-dynamic-mf/src/lib/models/module-definitions.type.ts
@@ -1,5 +1,12 @@
 import { ModuleDefinition } from './module-definition.type';
 
 export type ModuleDefinitions = {
+  /**
+   * The schema of the module definition. Should be 'https://raw.githubusercontent.com/LoaderB0T/ng-dynamic-mf/refs/heads/main/schema.json' or similar.
+   */
+  $schema: string;
+  /**
+   * The modules that should be loaded from the host application.
+   */
   modules: ModuleDefinition[];
 };
